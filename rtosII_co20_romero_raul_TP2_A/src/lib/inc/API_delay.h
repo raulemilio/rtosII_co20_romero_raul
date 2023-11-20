18fd969747a0c923f32e81ea3e089a6ae3f0c94c
rome <rraulemilioromero@gmail.com>
2023-11-20 09:36:46 -0300

    agregado de ejercicio 2

---
+++ rtosII_co20_romero_raul_TP2_A/src/lib/inc/API_delay.h
@@ -0,0 +1,22 @@
+/*
+ * @date   : 13-11-23
+ * @author : Raúl Romero <rraulemilioromero@gmail.com>
+ * @version	v1.0.0
+ */
+
+
+#include "main.h"
+#include <stdbool.h>
+
+typedef uint32_t tick_t;
+typedef bool bool_t;
+
+typedef struct{
+   tick_t startTime;
+   tick_t duration;
+   bool_t running;
+} delay_t;
+
+void delayInit( delay_t * delay, tick_t duration );
+bool_t delayRead( delay_t * delay );
+void delayWrite( delay_t * delay, tick_t duration );
